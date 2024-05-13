0aaf5d1c261e184f80b1db35848a34b66f8c8cf5
RafaelRiveraRTC <Tropicalcone25@gmail.com>
2024-05-13 14:14:52 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Coffee_Shop_Midterm
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
